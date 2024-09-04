3f6afd0249227a2ed35da440fceefc109c12e950
Esraa Abdellatif <esraa.abd.a99@gmail.com>
2024-09-05 00:40:21 +0300

    Open Google Map

--- lib/core/presentation/screen/home_map_screen.dart
+++ lib/core/presentation/screen/home_map_screen.dart
@@ -12,7 +12,10 @@ class HomeMapScreen extends StatelessWidget {
       ),
       body: const GoogleMap(
         initialCameraPosition: CameraPosition(
-            target: LatLng(37.43296265331129, -122.08832357078792), zoom: 19),
+            bearing: 192.8334901395799,
+            target: LatLng(37.43296265331129, -122.08832357078792),
+            tilt: 59.440717697143555,
+            zoom: 19.151926040649414),
       ),
     );
   }
